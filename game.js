e8475321d2b076b4941e1b35bf51dac422a6545f
Ahmed Hamed <ahamed@li-4c4c4544-0033-4210-8031-b5c04f525733.ibm.com>
2025-07-27 13:59:17 -0400

    test spwan

--- game.js
+++ game.js
@@ -401,7 +401,7 @@ bestScoreText = this.bestScoreText = this.add.text(16, 56, 'Best: ' + highScore,
   this.time.addEvent({
     delay: 1000, loop: true,
     callback: () => {
-      if (gameStarted && !gameOver && !gamePaused) {
+      if (gameStarted && !gameOver && !gamePaused && this.scene.isActive()) {
         let ramp = getConfigRamp(GAME_CONFIG.SPEED_RAMP, score).perTick;
         speed = Math.min(speed + ramp, maxSpeed);
       }
@@ -416,18 +416,22 @@ bestScoreText = this.bestScoreText = this.add.text(16, 56, 'Best: ' + highScore,
     return interval + Phaser.Math.Between(-50, 50);
   }
 
-  if (spawnEvent) spawnEvent.remove(false);
-  spawnEvent = this.time.addEvent({
-    delay: getSpawnInterval(),
-    loop: true,
-    callback: () => {
-      if (gameStarted && !gameOver && !gamePaused) {
-        spawnObjects.call(this);
-        lastSpawnTimestamp = this.time.now;
-      }
-      spawnEvent.delay = getSpawnInterval();
+function scheduleSpawnLoop(scene) {
+  if (spawnEvent) spawnEvent.remove(false); // Clear any old spawns
+
+  function spawnNext() {
+    if (gameStarted && !gameOver && !gamePaused && scene.scene.isActive()) {
+      spawnObjects.call(scene);
+      lastSpawnTimestamp = scene.time.now;
     }
-  });
+
+    const delay = getSpawnInterval();
+    spawnEvent = scene.time.delayedCall(delay, spawnNext);
+  }
+
+  spawnNext(); // Kick off the loop
+}
+
 
   this.time.addEvent({
     delay: 250,
@@ -499,7 +503,7 @@ bestScoreText = this.bestScoreText = this.add.text(16, 56, 'Best: ' + highScore,
       obj === this.pauseIcon || obj === this.muteIcon
     )) return;
 
-    if (gameStarted && !gameOver && !gamePaused) {
+    if (gameStarted && !gameOver && !gamePaused && scene.scene.isActive()) {
       direction *= -1;
       sfx.move.play();
       this.tweens.add({
@@ -834,7 +838,11 @@ function handleStartGame() {
     scene.pauseIcon.setVisible(true);
     scene.muteIcon.setVisible(true);
     scene.startCountdown(function() {
-      gameStarted = true;
+    gameStarted = true;
+    scene.time.delayedCall(100, () => {
+      scheduleSpawnLoop(scene);
+    });
+
     });
   }, 200);
 }
@@ -899,7 +907,10 @@ function handlePlayAgain() {
     scene.pauseIcon.setVisible(true);
     scene.muteIcon.setVisible(true);
     scene.startCountdown(function() {
-      gameStarted = true;
+    gameStarted = true;
+    scene.time.delayedCall(100, () => {
+      scheduleSpawnLoop(scene);
+    });
     });
   }, 0);
 }
